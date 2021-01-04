477e8bc7a8d0fb40b5d022bf53eaf9bad5c5215c
Сафонов Александр <izyakantorovich@gmail.com>
2021-01-04 22:18:46 +0300

    Remove namespace command_processor

--- src/command_processor/command_processor.h
+++ src/command_processor/command_processor.h
@@ -7,37 +7,32 @@
 
 
 
-namespace command_processor
+using command_mnemonic = const string;
+
+
+
+
+enum class MotionMode { FREE_DESCES, WITH_DESCES, RECONNECT_DESCES };
+
+inline bool isMotionModeString (const string & str)
 {
-
-	using command_mnemonic = const string;
-
-
-
-
-	enum class MotionMode { FREE_DESCES, WITH_DESCES, RECONNECT_DESCES };
-
-	inline bool isMotionModeString (const string & str)
-	{
-		if (str != "f" && str != "w" && str != "r")    return false;
-		return true;
-	}
-
-	inline MotionMode parseMotionMode (const string & str)
-	{
-		if (!isMotionModeString(str))
-			throw exception(  string("\"" + str + "\" is not a mode of deleting").c_str()  );
-
-		if (str == "w")
-			return MotionMode::WITH_DESCES;
-		if (str == "f")
-			return MotionMode::FREE_DESCES;
-		return MotionMode::RECONNECT_DESCES;
-	}
-
-	
-	
-	
-	void executeCommand (string );
-
+	if (str != "f" && str != "w" && str != "r")    return false;
+	return true;
 }
+
+inline MotionMode parseMotionMode (const string & str)
+{
+	if (!isMotionModeString(str))
+		throw exception(  string("\"" + str + "\" is not a mode of deleting").c_str()  );
+
+	if (str == "w")
+		return MotionMode::WITH_DESCES;
+	if (str == "f")
+		return MotionMode::FREE_DESCES;
+	return MotionMode::RECONNECT_DESCES;
+}
+
+
+
+
+void executeCommand (string );
